62593bdd787fb00a9b2bcdad059b56ab198f458c
Sabri_Efe <se.zybk@hotmail.com>
2026-03-23 12:02:09 +0300

    added

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function isEnoughCapacity(products, containerSize){
+    let total = 0;
+    const val = Object.values(products);
+    for(const i of val){
+        total += i;
+    }
+    return total <= containerSize;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName){
+        this.username = newName;
+    },
+    updatePlayTime(saatler){
+        this.playTime = saatler;
+    },
+    getInfo(){
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
+
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
